5b2e795f0557ed4746529e608826b612fb9a91c1
warren gnahoui <warren.gnahoui@epitech.eu>
2023-04-19 15:23:45 +0100

    removing dummy url file

--- backend/visitors/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from visitors.views import (
-    RegistrationView,
-)
-
-
-urlpatterns = [
-    path('register/', RegistrationView.as_view(), name="register_visitor"),
-]
